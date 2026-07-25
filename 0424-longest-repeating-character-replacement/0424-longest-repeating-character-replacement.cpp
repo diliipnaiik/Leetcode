c08573aded86c84d4ad35ca187f55e929ddc2fd5
Dilip Naika K <dilipnaika.cs24@bmsce.ac.in>
2026-07-25 12:18:22 +0530

    Time: 11 ms (29.69%), Space: 11 MB (42.01%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map <char,int> window;
+
+        int ans = 0;
+        int left = 0;
+        int maxFreq = 0;
+
+        for(int right = 0 ; right < s.size() ; right++){
+            window[s[right]]++;
+
+            maxFreq = max(maxFreq,window[s[right]]);
+
+            while((right - left + 1) - maxFreq > k ){
+                window[s[left]]--;
+                left++;
+            }
+            
+            ans = max(right - left +1 , ans);
+        }
+        return ans;
+    }
+};
