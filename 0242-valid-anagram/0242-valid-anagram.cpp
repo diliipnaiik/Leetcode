2452e0fc4c11e2e80f1950cc2f476e64c6bfe1cb
Dilip Naika K <dilipnaika.cs24@bmsce.ac.in>
2026-07-25 12:21:19 +0530

    Time: 6 ms (22.59%), Space: 10 MB (11.56%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<int,int> mp;
+        int len1 = s.size();
+        int len2 = t.size();
+
+        if(len1 != len2){
+            return false;
+        }
+
+        for(char ch : s){
+            mp[ch]++;
+        }
+
+        for(char ch : t){
+            mp[ch]--;
+
+            if(mp[ch] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
