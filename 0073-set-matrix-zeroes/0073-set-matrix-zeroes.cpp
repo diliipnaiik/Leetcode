2817d2390169e80f469187b9a15327a6335776b7
Dilip Naika K <dilipnaika.cs24@bmsce.ac.in>
2026-08-01 22:21:41 +0530

    Time: 0 ms (100%), Space: 20.9 MB (30%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -5,40 +5,37 @@ public:
         int colSize = matrix[0].size();
         int col = 1;
 
-        // Mark rows and columns that need to be zeroed
-        for (int i = 0; i < rowSize; i++) {
-            for (int j = 0; j < colSize; j++) {
-                if (matrix[i][j] == 0) {
+        for(int i = 0 ; i < rowSize ; i++){
+            for(int j = 0 ; j < colSize ; j++){
+                if(matrix[i][j] == 0){
                     matrix[i][0] = 0;
 
-                    if (j != 0) {
+                    if(j != 0){
                         matrix[0][j] = 0;
-                    } else {
-                        col = 0;
                     }
+                    else{
+                        col = 0;
+                    }   
                 }
             }
         }
 
-        // Update the inner matrix
-        for (int i = 1; i < rowSize; i++) {
-            for (int j = 1; j < colSize; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        for(int i = 1 ; i < rowSize ; i++){
+            for(int j = 1 ; j < colSize ; j++){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
 
-        // Update the first row
-        if (matrix[0][0] == 0) {
-            for (int j = 0; j < colSize; j++) {
+        if(matrix[0][0] == 0){
+            for(int j = 0 ; j < colSize ; j++){
                 matrix[0][j] = 0;
             }
         }
 
-        // Update the first column
-        if (col == 0) {
-            for (int i = 0; i < rowSize; i++) {
+        if(col == 0){
+            for(int i = 0 ; i < rowSize ; i++){
                 matrix[i][0] = 0;
             }
         }
