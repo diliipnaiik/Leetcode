65e8c56a06d8c3d037a162b0d664bc5086b6a623
Dilip Naika K <dilipnaika.cs24@bmsce.ac.in>
2026-08-01 23:33:25 +0530

    Time: 11 ms (47.76%), Space: 18.4 MB (8.34%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map <char,int> window;
+        vector<int> lastSeen(128,-1);
         int left = 0;
         int ans = 0;
 
         for(int right = 0 ; right < s.size() ; right++){
-            if(window.find(s[right]) != window.end() && window[s[right]] >= left){
-                left = window[s[right]] + 1;
+            if(lastSeen[s[right]] >= left){
+                left = lastSeen[s[right]] + 1;
             }
 
-            window[s[right]] = right;
+            lastSeen[s[right]] = right;
 
-            ans = max(ans,right - left + 1);
-        }
+            ans = max(right - left + 1, ans);
+        }   
         return ans;
     }
 };
