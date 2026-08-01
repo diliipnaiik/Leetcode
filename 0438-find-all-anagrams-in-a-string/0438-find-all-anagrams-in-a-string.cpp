b5e9140accfa08abe07625615518d5efd247b721
Dilip Naika K <dilipnaika.cs24@bmsce.ac.in>
2026-08-02 01:46:30 +0530

    Time: 3 ms (81.17%), Space: 11.6 MB (36.89%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -2,40 +2,39 @@ class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
         vector<int> ans;
-        unordered_map <char,int> need;
-        unordered_map <char,int> window;
+        vector<int> window(26,0);
+        vector<int> need(26,0);
 
-        for(char n : p){
-            need[n]++;
+        if(p.size() > s.size()){
+            return {};
+        }
+
+        for(char c : p){
+            need[c - 'a']++;
         }
 
         int windowSize = p.size();
 
-        int left = 0;
-
         for(int i = 0 ; i < windowSize ; i++){
-            window[s[i]]++;
+            window[s[i] - 'a']++;
         }
+
+        int left = 0;
 
         if(window == need){
             ans.push_back(left);
         }
 
         for(int right = windowSize ; right < s.size() ; right++){
-            window[s[left]]--;
+            window[s[right] - 'a']++;
 
-            if(window[s[left]] == 0){
-                window.erase(s[left]);
-            }
-
+            window[s[left] - 'a']--;
             left++;
-
-            window[s[right]]++;
 
             if(window == need){
                 ans.push_back(left);
             }
         }
-        return ans;
+        return ans; 
     }
 };
