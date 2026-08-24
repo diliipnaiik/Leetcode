a65ab339eec26f664d1252fec4b7ae079a731575
Dilip Naika K <dilipnaika.cs24@bmsce.ac.in>
2026-08-24 13:24:54 +0530

    Time: 162 ms (8.89%), Space: 139.1 MB (92.39%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,16 +2,22 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> ans;
-        priority_queue<pair<int,int>> maxHeap;
+        deque<int> dq;
 
         for(int i = 0 ; i < nums.size() ; i++){
-            maxHeap.push({nums[i],i});
+
+            while(!dq.empty() && dq.front() < i - k + 1){
+                dq.pop_front();
+            }
+
+            while(!dq.empty() && nums[dq.back()] < nums[i]){
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
 
             if(i >= k - 1){
-                while(maxHeap.top().second < i - k + 1){
-                    maxHeap.pop();
-                }
-                ans.push_back(maxHeap.top().first);
+                ans.push_back(nums[dq.front()]);
             }
         }
         return ans;
