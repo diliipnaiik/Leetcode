788e50b391662e41cc638103c09546847d7e33f5
Dilip Naika K <dilipnaika.cs24@bmsce.ac.in>
2026-08-26 23:33:00 +0530

    Time: 4 ms (42.49%), Space: 18.3 MB (82.71%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -11,23 +11,22 @@
 class Solution {
 private:
     struct compare{
-        bool operator() (const ListNode* a, const ListNode* b){
+        bool operator() (const ListNode* a , const ListNode* b){
             return a->val > b->val;
         }
     };
-
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode* , vector<ListNode*> , compare> minHeap;
+       priority_queue<ListNode* , vector<ListNode*>,compare> minHeap;
 
         for(ListNode* root : lists){
             if(root != nullptr){
                 minHeap.push(root);
             }
-        }
-        
+        }   
+
         ListNode dummy(0);
-        ListNode (*tail) = &dummy;
+        ListNode* tail = &dummy;
 
         while(!minHeap.empty()){
             ListNode* smallest = minHeap.top();
@@ -37,7 +36,7 @@ public:
             tail = tail -> next;
 
             if(smallest -> next != nullptr){
-                minHeap.push(smallest->next);
+                minHeap.push(smallest -> next);
             }
         }
         return dummy.next;
