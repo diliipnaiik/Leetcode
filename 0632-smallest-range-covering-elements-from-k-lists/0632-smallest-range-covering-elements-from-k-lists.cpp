85b1ad5ff228efe8325ce5dc6c9135e2e9640395
Dilip Naika K <dilipnaika.cs24@bmsce.ac.in>
2026-08-27 22:57:33 +0530

    Time: 309 ms (10.43%), Space: 73.5 MB (17.42%) - LeetHub

---
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        priority_queue<vector<int> , vector<vector<int>> , greater<vector<int>>> minHeap;
+        int range_start = 0;
+        int range_end = INT_MAX;
+        int current_max = INT_MIN;
+
+        for(int i = 0 ; i < nums.size() ; ++i){
+            minHeap.push({nums[i][0],i,0});
+            current_max = max(current_max,nums[i][0]);
+        }
+
+        while(minHeap.size() == nums.size()){
+            auto curr = minHeap.top();
+            minHeap.pop();
+
+            int current_min = curr[0];
+            int listIdx = curr[1];
+            int eleIdx = curr[2];
+
+            if((long long) current_max - current_min < (long long)range_end - range_start){
+                range_start = current_min;
+                range_end = current_max;
+            }
+
+            if(eleIdx + 1 < nums[listIdx].size()){
+                auto nextVal = nums[listIdx][eleIdx+1];
+                minHeap.push({nextVal,listIdx,eleIdx+1});
+                current_max = max(current_max,nextVal);
+            }
+            else{
+                break;
+            }
+        }
+        return {range_start,range_end};
+    }
+};
