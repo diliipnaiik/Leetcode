fee9d8d531a867631c51bd221bd76ca7fb2d4a7b
Dilip Naika K <dilipnaika.cs24@bmsce.ac.in>
2026-08-28 22:03:39 +0530

    Time: 300 ms (19.3%), Space: 73.3 MB (22.08%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -6,8 +6,8 @@ public:
         int range_end = INT_MAX;
         int current_max = INT_MIN;
 
-        for(int i = 0 ; i < nums.size() ; ++i){
-            minHeap.push({nums[i][0],i,0});
+        for(int i = 0 ; i < nums.size() ; i++){
+           minHeap.push({nums[i][0],i,0});
             current_max = max(current_max,nums[i][0]);
         }
 
@@ -19,20 +19,17 @@ public:
             int listIdx = curr[1];
             int eleIdx = curr[2];
 
-            if((long long) current_max - current_min < (long long)range_end - range_start){
+            if((long long )current_max - current_min < (long long)range_end - range_start){
                 range_start = current_min;
                 range_end = current_max;
             }
 
             if(eleIdx + 1 < nums[listIdx].size()){
-                auto nextVal = nums[listIdx][eleIdx+1];
+                int nextVal = nums[listIdx][eleIdx+1];
                 minHeap.push({nextVal,listIdx,eleIdx+1});
                 current_max = max(current_max,nextVal);
             }
-            else{
-                break;
-            }
         }
-        return {range_start,range_end};
+        return {range_start , range_end};
     }
 };
