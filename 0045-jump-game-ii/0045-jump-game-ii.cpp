268b490e2c37e3ef6d315769b92638a92955c93e
Dilip Naika K <dilipnaika.cs24@bmsce.ac.in>
2026-08-29 13:18:50 +0530

    Time: 0 ms (100%), Space: 20.6 MB (78.62%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps = 0;
+        int curr_end = 0;
+        int max_Reach = INT_MIN;
+
+        for(int i = 0 ; i < nums.size() - 1; ++i){
+            max_Reach = max(max_Reach,nums[i]+i);
+
+            if(curr_end == i){
+                jumps++;
+                curr_end = max_Reach;
+
+                if(curr_end >= nums.size() - 1){
+                    return jumps;
+                }
+            }
+        }
+        return jumps;
+    }
+};
