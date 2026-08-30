9e8934d5d77fce98119be38d61818e2ebe02fd9f
Dilip Naika K <dilipnaika.cs24@bmsce.ac.in>
2026-08-30 21:47:36 +0530

    Time: 3 ms (34.8%), Space: 112.4 MB (61.49%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -3,19 +3,20 @@ public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int totalCost = 0;
         int totalGas = 0;
-        int startIdx = 0;
         int currentTank = 0;
+        int startIndex = 0;
 
         for(int i = 0 ; i < gas.size() ; i++){
             totalCost += cost[i];
             totalGas += gas[i];
-            currentTank += gas[i] - cost[i];
+            currentTank += cost[i] - gas[i];
 
-            if(currentTank < 0){
-                startIdx = i + 1;
+            if(currentTank > 0){
+                startIndex = i + 1;
                 currentTank = 0;
             }
         }
-        return (totalGas < totalCost) ? -1 : startIdx;
+
+        return (totalGas < totalCost) ? -1 : startIndex;
     }
 };
