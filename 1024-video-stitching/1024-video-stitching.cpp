a00c43334d8c5d7a182be1e6521f8ed9580c5ca4
Dilip Naika K <dilipnaika.cs24@bmsce.ac.in>
2026-08-30 21:29:49 +0530

    Time: 0 ms (100%), Space: 12.8 MB (5.02%) - LeetHub

--- 1024-video-stitching/1024-video-stitching.cpp
+++ 1024-video-stitching/1024-video-stitching.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     int videoStitching(vector<vector<int>>& clips, int time) {
-        sort(clips.begin(), clips.end() , [](const vector<int> a, const vector<int> b){
+        sort(clips.begin(), clips.end() , [] (const vector<int> a, const vector<int> b){
             return a[0] < b[0];
         });
 
-        int cut = 0;
         int maxReach = INT_MIN;
-        int curr_End = 0;
-        int n = clips.size();
+        int cuts = 0;
+        int curr_end = 0;
         int i = 0;
+        int n = clips.size();
 
-        while(curr_End < time){
-            while(i < n && clips[i][0] <= curr_End){
-                maxReach = max(maxReach , clips[i][1]);
+        while(curr_end < time){
+            while(i < n && clips[i][0] <= curr_end){
+                maxReach = max(clips[i][1],maxReach);
                 i++;
             }
 
-            if(maxReach == curr_End){
+            if(curr_end == maxReach){
                 return -1;
             }
 
-            curr_End = maxReach;
-            cut++;
+            curr_end = maxReach;
+            cuts++;
         }
-        return cut;
+        return cuts;
     }
 };
